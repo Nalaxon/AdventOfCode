db0bbb40dd937325790da5d17edf104bf265551f
Bernhard Rapp <boerny@linuxtage.at>
2024-12-01 23:14:46 +0100

    1 a

---
+++ 2024/Run.java
@@ -0,0 +1,34 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.stream.IntStream;
+
+
+
+public class Run {
+    public static void main(String[] args) {
+        String fileName = "day1.txt";
+        File file = new File(fileName);
+        final var left = new ArrayList<Integer>();
+        final var right = new ArrayList<Integer>();
+
+        try {
+        Files.lines(file.toPath())
+        .forEach(line -> {
+            final String[] values = line.split("   ");
+            left.add(Integer.parseInt(values[0]));
+            right.add(Integer.parseInt(values[1]));
+
+        });
+        } catch(final Exception e) {
+            System.out.println(e);
+        }
+
+        final var rList = right.stream().sorted().toList();
+        final var lList = left.stream().sorted().toList();
+        final var range = IntStream.range(0, rList.size());
+        System.out.println(range.map(i -> Math.abs(rList.get(i) - lList.get(i)))
+        .sum());
+}
+
+}
